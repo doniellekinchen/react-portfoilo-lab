12b42cc73664226bc0586e8a47c9712244f7df4f
doniellekinchen <donicodes.things@gmail.com>
2023-12-13 15:25:44 -0500

    app.jsx errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
-// IMPORT COMPONENTS
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+// Import Components
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-// IMPORT PAGES
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
+// Import Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
-
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
       </Routes>
